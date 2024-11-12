42587c5fd489ec256ef8448468b521dce5838293
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-12 13:57:00 +0100

    Apply custom error handling with logger

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
+const { errorHandler } = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
@@ -10,7 +11,7 @@ app.use(logger);
 const whitelist = ["https://www.yoursite.com", "http://localhost:3500"];
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
@@ -41,5 +42,7 @@ app.get("/old-page(.html)?", (req, res) => {
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
