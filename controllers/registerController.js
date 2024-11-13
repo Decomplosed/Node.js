6864b25171e9c8674b17fcaba6310e878d4a20aa
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-13 10:19:32 +0100

    Created endpoint for registering a user

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,3 +7,39 @@ const usersDB = {
 
 const fsPromises = require("fs").promises;
 const path = require("path");
+const bcrypt = require("bcrypt");
+
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body;
+
+  if (!user || !pwd) {
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+  }
+
+  const duplicate = usersDB.users.find(
+    (existingUser) => existingUser.username === user
+  );
+
+  if (duplicate) {
+    return res.sendStatus(409);
+  }
+
+  try {
+    const hashedPwd = await bcrypt.hash(pwd, 10);
+    const newUser = { username: user, password: hashedPwd };
+    usersDB.setUsers([...usersDB.users, newUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    res.status(201).json({ success: `New user ${user} created!` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  handleNewUser,
+};
